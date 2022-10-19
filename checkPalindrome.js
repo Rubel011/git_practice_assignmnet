aa262b2fe581aa4692bd96c8bff5a30c1259ae78
RahulRathod5181 <rahulrathod5181@gmail.com>
2022-10-19 23:49:57 +0530

    I have fixed the code

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,10 +1,11 @@
 let str="madam"
 let bag="";
-for(let i=str.length-1;i>=0;i++){
+for(let i=str.length-1;i>=0;i--){
   bag+=str[i];
   }
-if(bag=str){
+if(bag==str){
   console.log("Yes");
-  }else{
+  }
+else{
         console.log("No");
-      }
+    }
